368c7c0df616cc470bd52552f8f9f1854c6dab7c
Luca Ongaro <lukeongaro@gmail.com>
2012-05-05 16:33:08 +0200

    use $.extend to extend jQuery

--- jquery-toCurrency/jquery-tocurrency.0.1.1.js
+++ jquery-toCurrency/jquery-tocurrency.0.1.1.js
@@ -8,46 +8,48 @@
  */ 
 (function( $ ){
   "use strict";
-  $.toCurrency = function( val, opts ) {
-    var val_str, reverse_whole_part, decimals, i,
-        sign = "",
-        whole_part = "",
-        default_options = {
-          precision: 2,
-          delimiter: ",",
-          separator: ".",
-          unit: "&euro;",
-          format: "%u %n",
-          negativeFormat: false
-        };
-    opts = $.extend( default_options, opts );
+  $.extend({
+    toCurrency: function( val, opts ) {
+      var val_str, reverse_whole_part, decimals, i,
+          sign = "",
+          whole_part = "",
+          default_options = {
+            precision: 2,
+            delimiter: ",",
+            separator: ".",
+            unit: "&euro;",
+            format: "%u %n",
+            negativeFormat: false
+          };
+      opts = $.extend( default_options, opts );
 
-    val = parseFloat(val);
-    if( isNaN(val) ) {
-      throw new Error( "toCurrency error: invalid number format " + val );
-    }
-    if ( val < 0 ) {
-      sign = "-";
-    }
-
-    val_str = val.toFixed( opts.precision ).replace( "-", "" );
-    reverse_whole_part = val_str.split(".")[0].split("").reverse().join("");
-    decimals = ( opts.precision > 0 ? opts.separator : "" ) + ( val_str.split(".")[1] || "" );
-        
-    for ( i = 0; i < reverse_whole_part.length; i++ ) {
-      if ( i % 3 === 0 && i !== 0 ) {
-        whole_part += opts.delimiter;
+      val = parseFloat(val);
+      if( isNaN(val) ) {
+        throw new Error( "toCurrency error: invalid number format " + val );
       }
-      whole_part += reverse_whole_part.charAt(i);
+      if ( val < 0 ) {
+        sign = "-";
+      }
+
+      val_str = val.toFixed( opts.precision ).replace( "-", "" );
+      reverse_whole_part = val_str.split(".")[0].split("").reverse().join("");
+      decimals = ( opts.precision > 0 ? opts.separator : "" ) + ( val_str.split(".")[1] || "" );
+          
+      for ( i = 0; i < reverse_whole_part.length; i++ ) {
+        if ( i % 3 === 0 && i !== 0 ) {
+          whole_part += opts.delimiter;
+        }
+        whole_part += reverse_whole_part.charAt(i);
+      }
+      whole_part = whole_part.split("").reverse().join("");
+      
+      if ( sign === "-" && opts.negativeFormat ) {
+        return opts.negativeFormat.replace( "%n", whole_part + decimals ).replace( "%u", opts.unit );
+      } else {
+        return opts.format.replace( "%n", sign + whole_part + decimals ).replace( "%u", opts.unit );
+      }
     }
-    whole_part = whole_part.split("").reverse().join("");
-    
-    if ( sign === "-" && opts.negativeFormat ) {
-      return opts.negativeFormat.replace( "%n", whole_part + decimals ).replace( "%u", opts.unit );
-    } else {
-      return opts.format.replace( "%n", sign + whole_part + decimals ).replace( "%u", opts.unit );
-    }
-  };
+  });
   
   $.fn.toCurrency = function( options ) {
     return this.each(function() {
